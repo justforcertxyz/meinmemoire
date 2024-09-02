0d6b69b37b11a89469ed1b11b56f1c21262656ca
ok <ok@ohhkay.de>
2024-09-03 00:13:07 +0200

    add Memoire, BasePlaneMemoire and PremiumPlanMemoire model

--- meinmemoire/memoire/models.py
+++ meinmemoire/memoire/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth import get_user_model
+from django.conf import settings
 
-# Create your models here.
+User = get_user_model()
+
+
+class Memoire(models.Model):
+    creation_date = models.DateTimeField(
+        "Creation Date", default=timezone.now)
+    user = models.ForeignKey(User, verbose_name="User",
+                             on_delete=models.CASCADE)
+
+    subscription_price = None
+
+    @classmethod
+    def create(cls, user):
+        return cls.objects.create(user=user)
+
+
+class BasePlanMemoire(Memoire):
+    pass
+
+
+class PremiumPlanMemoire(Memoire):
+    pass
--- meinmemoire/memoire/tests.py
+++ meinmemoire/memoire/tests.py
@@ -1,3 +1,69 @@
 from django.test import TestCase
+from .models import Memoire, BasePlanMemoire, PremiumPlanMemoire
+from django.contrib.auth import get_user_model
+from django.conf import settings
 
-# Create your tests here.
+User = get_user_model()
+
+
+class MemoireModelTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username="Foo", password="bar")
+        self.memoire_type = Memoire
+
+    def test_model_exists(self):
+        self.assertTrue(issubclass(self.memoire_type, Memoire))
+
+        count = self.memoire_type.objects.count()
+        self.assertEqual(count, 0)
+
+    def test_create(self):
+        memoire = self.memoire_type.create(user=self.user)
+        self.assertTrue(isinstance(memoire, Memoire))
+
+        count = self.memoire_type.objects.count()
+        self.assertEqual(count, 1)
+
+        self.assertEqual(memoire.user, self.user)
+
+
+class BasePlanMemoireModelTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username="Foo", password="bar")
+        self.memoire_type = BasePlanMemoire
+
+    def test_model_exists(self):
+        self.assertTrue(issubclass(self.memoire_type, Memoire))
+
+        count = self.memoire_type.objects.count()
+        self.assertEqual(count, 0)
+
+    def test_create(self):
+        memoire = self.memoire_type.create(user=self.user)
+        self.assertTrue(isinstance(memoire, BasePlanMemoire))
+
+        count = self.memoire_type.objects.count()
+        self.assertEqual(count, 1)
+
+        self.assertEqual(memoire.user, self.user)
+
+
+class PremiumPlanMemoireModelTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username="Foo", password="bar")
+        self.memoire_type = PremiumPlanMemoire
+
+    def test_model_exists(self):
+        self.assertTrue(issubclass(self.memoire_type, Memoire))
+
+        count = self.memoire_type.objects.count()
+        self.assertEqual(count, 0)
+
+    def test_create(self):
+        memoire = self.memoire_type.create(user=self.user)
+        self.assertTrue(isinstance(memoire, PremiumPlanMemoire))
+
+        count = self.memoire_type.objects.count()
+        self.assertEqual(count, 1)
+
+        self.assertEqual(memoire.user, self.user)
