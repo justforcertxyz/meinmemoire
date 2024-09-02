a4bf0a530a662b7e2617102bd3536a52a079b6f1
ok <ok@ohhkay.de>
2024-09-02 22:57:44 +0200

    add landing:index

--- meinmemoire/landing/tests.py
+++ meinmemoire/landing/tests.py
@@ -1,3 +1,18 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
 
-# Create your tests here.
+
+class IndexPageTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.url = reverse('landing:index')
+
+    def test_index_page_returns_correct_response(self):
+        response = self.client.get(self.url)
+        self.assertTemplateUsed(response, 'landing/index.html')
+        self.assertTemplateUsed(response, 'landing/base.html')
+        self.assertEqual(response.status_code, 200)
+
+    def test_index_page_returns_corrent_content(self):
+        response = self.client.get(self.url)
+        self.assertContains(response, "<title>Home")
